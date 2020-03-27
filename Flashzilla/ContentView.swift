908aa6d9af69e5d111c1d61d72d712d16cf68454
Davron <devrannur@gmail.comn>
2020-03-27 07:59:20 -0500

    onLongPressGesture

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -11,6 +11,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
+            .onLongPressGesture(minimumDuration: 1, pressing: { inProgress in
+                print("In progress: \(inProgress)!")
+            }) {
+                print("Long pressed!")
+            }
     }
 }
 
